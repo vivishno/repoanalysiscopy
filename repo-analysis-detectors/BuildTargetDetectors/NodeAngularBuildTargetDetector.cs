a80629ca9512eecaf2b7d84f9d7bb601386acf32
Ninad Kavimandan <ninadkavimandan@github.com>
2020-11-12 10:58:17 +0530

    added angular

---
+++ repo-analysis-detectors/BuildTargetDetectors/NodeAngularBuildTargetDetector.cs
@@ -0,0 +1,82 @@
+﻿namespace Microsoft.VisualStudio.PortalExtension.Server.Services.RepositoryAnalysis.Detectors.BuildTargetDetectors
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.PortalExtension.Server.Services.RepositoryAnalysis.Detectors.Models;
+
+    /// <summary>
+    /// Build target detector for Vue build target for Node projects
+    /// </summary>
+    public class NodeAngularBuildTargetDetector : BuildTargetDetectorBase
+    {
+        /// <summary>
+        /// Build target name
+        /// </summary>
+        public override string BuildTargetName => Constants.AngularBuildTargetName;
+        private List<string> AngularProjectFolders;
+
+        /// <summary>
+        /// Constructor for the build target detector
+        /// </summary>
+        public NodeAngularBuildTargetDetector()
+        {
+            AngularProjectFolders = new List<string>();
+        }
+
+        /// <summary>
+        /// Returns list of build target settings for the Angular build target.
+        /// </summary>
+        /// <param name="treeAnalysis"></param>
+        /// <returns>List of build target settings for Angular</returns>
+        public override List<BuildTargetSettings> GetBuildTargetSettings(TreeAnalysis treeAnalysis)
+        {
+            List<BuildTargetSettings> buildTargetSettings = new List<BuildTargetSettings>();
+            foreach (var angularProjectFolder in AngularProjectFolders)
+            {
+                buildTargetSettings.Add(
+                    new BuildTargetSettings
+                    {
+                        Name = BuildTargetName,
+                        Settings = new Dictionary<string, object>
+                        {
+                            { Constants.WorkingDirectory, angularProjectFolder },
+                            { Constants.AppArtifactLocation, $"{angularProjectFolder}/{Constants.AngularBuildArtifactPath}" },
+                            { Constants.PackageJsonFilePath, $"{angularProjectFolder}/{Constants.PackageJsonFileName}" }
+                        }
+                    }
+                );
+            }
+            return buildTargetSettings;
+        }
+
+        /// <summary>
+        /// Checks if repo contains Vue build target projects, using treeAnalysis object.
+        /// </summary>
+        /// <param name="treeAnalysis"></param>
+        /// <returns>true on Vue build target detection, otherwise false</returns>
+        public override bool IsBuildTargetDetected(TreeAnalysis treeAnalysis)
+        {
+            FindAndAddVueProjectFolders(treeAnalysis);
+            if (AngularProjectFolders.Count > 0)
+                return true;
+            return false;
+        }
+
+        internal void FindAndAddVueProjectFolders(TreeAnalysis treeAnalysis)
+        {
+            List<FileSystemTreeNode> packageJsonNodes = new List<FileSystemTreeNode>(treeAnalysis.FilesInfo[Constants.PackageJsonFileName]);
+
+            foreach (var packageJsonNode in packageJsonNodes)
+            {
+                if (HasAngularDependency(treeAnalysis.FilesContent[packageJsonNode.Path].ToObject<PackageJsonModel>()))
+                    AngularProjectFolders.Add(GetDirectoryPath(packageJsonNode));
+            }
+        }
+
+        internal bool HasAngularDependency(PackageJsonModel packageJson)
+        {
+            if (packageJson.dependencies.ContainsKey(Constants.AngularPackageName))
+                return true;
+            return false;
+        }
+    }
+}
--- repo-analysis-detectors/LanguageDetectors/LanguageDetectorBase.cs
+++ repo-analysis-detectors/LanguageDetectors/LanguageDetectorBase.cs
@@ -46,7 +46,8 @@ namespace Microsoft.VisualStudio.PortalExtension.Server.Services.RepositoryAnaly
                     return new List<IBuildTargetDetector>
                     {
                         new NodeReactBuildTargetDetector(),
-                        new NodeVueBuildTargetDetector()
+                        new NodeVueBuildTargetDetector(),
+                        new NodeAngularBuildTargetDetector()
                     };
             }
             return new List<IBuildTargetDetector> { };
